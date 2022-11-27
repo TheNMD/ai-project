59cf6f74b96208da72d340c891fcff95cda9d889
TheNMD <manhdanog@gmail.com>
2022-11-27 20:47:25 +0700

    com

--- digitreg.py
+++ digitreg.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import pandas as pd
 
-csv = pd.read_csv('./data/csv/train.csv')
-train_image = csv.iloc[0:10000, 1:]
-test_image = csv.iloc[10001:20000, 1:]
-train_label = csv.iloc[10001:20000, :1]
+(image_train, label_train), (image_test, label_test) = tf.keras.datasets.mnist.load_data()
+
+print(y_train)
