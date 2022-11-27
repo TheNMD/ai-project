c642dabda2407a0d673c4e814c9af5599df88475
TheNMD <manhdanog@gmail.com>
2022-11-27 19:05:19 +0700

    test

--- digitreg.py
+++ digitreg.py
@@ -1,3 +1,8 @@
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
+
+csv = pd.read_csv('./data/csv/train.csv')
+train_image = csv.iloc[0:10000, 1:]
+test_image = csv.iloc[10001:20000, 1:]
+train_label = csv.iloc[10001:20000, :1]
