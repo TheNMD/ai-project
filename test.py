c2258f98d0a07e273d32097b57aca447058eb48a
TheNMD <manhdanog@gmail.com>
2022-11-28 17:42:27 +0700

    m

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-import json, scipy
+import numpy as np
+
+def softmax(data):
+    e = np.exp(data)
+    return e / sum(e)
+    
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+print(softmax(arr))
+print(sum(softmax(arr)))
