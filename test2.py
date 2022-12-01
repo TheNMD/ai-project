001222de44f6b23cc2bed9ac08e434bcb4839a7c
TheNMD <manhdanog@gmail.com>
2022-12-02 00:59:55 +0700

    com

--- test2.py
+++ test2.py
@@ -1,10 +1,4 @@
 import numpy as np
 
-x = np.random.rand(10, 3)
-w = np.random.rand(10, 5)
-b = np.random.rand(5, 1)
-y = np.dot(w.T, x) + b
-
-print(np.dot(w.T, x))
-print(b)
-print(y)
+x = np.random.randint(55000, size=32)
+print(x)
